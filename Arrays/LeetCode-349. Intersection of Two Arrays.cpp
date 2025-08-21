132b58892e141857624faa7239af09a0fc68369f
Yash Pathak <129614416+yashpathak-007@users.noreply.github.com>
2025-08-21 23:56:25 +0530

    Update LeetCode-349. Intersection of Two Arrays.cpp

--- Arrays/LeetCode-349. Intersection of Two Arrays.cpp
+++ Arrays/LeetCode-349. Intersection of Two Arrays.cpp
@@ -23,31 +23,51 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
+    //     vector<int> ans;
 
-        for(int i = 0 ; i < nums1.size(); i++){
-            int element = nums1[i];
+    //     for(int i = 0 ; i < nums1.size(); i++){
+    //         int element = nums1[i];
 
-            bool isPresent = false;
-            for(int k = 0 ; k < ans.size() ;  k++){
-                if(ans[k] == element){
-                    isPresent = true;
-                    break;
-                }
-            }
+    //         bool isPresent = false;
+    //         for(int k = 0 ; k < ans.size() ;  k++){
+    //             if(ans[k] == element){
+    //                 isPresent = true;
+    //                 break;
+    //             }
+    //         }
                 
-            if(isPresent) continue;
+    //         if(isPresent) continue;
 
-            //checking == with the i th index value to all values of the 2nd array.
-            for(int j = 0 ; j < nums2.size() ; j++){
-                if(element == nums2[j]){
-                    ans.push_back(nums1[i]);
-                    nums1[i] = -1;
-                   break;
-                }
-            }
+    //         //checking == with the i th index value to all values of the 2nd array.
+    //         for(int j = 0 ; j < nums2.size() ; j++){
+    //             if(element == nums2[j]){
+    //                 ans.push_back(nums1[i]);
+    //                 nums1[i] = -1;
+    //                break;
+    //             }
+    //         }
             
+    //     }
+    //     return ans;
+    
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector<int> ans;
+        
+        int i=0, j = 0;
+        
+        while(i< nums1.size() &&  j < nums2.size()){
+            if(nums1[i]==nums2[j]){
+                if(ans.empty() || ans.back()!=nums1[i]){
+                ans.push_back(nums1[i]);
+                }
+                i++;j++;
+            }
+            else if(nums1[i]<nums2[j])
+                i++;
+            else
+                j++;
         }
-        return ans;
+    return ans;
     }
 };
